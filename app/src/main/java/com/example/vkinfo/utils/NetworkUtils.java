758d66fe1eb66cf6b78650dc17b39958f9686afc
Stepan Mednikov <x-steff@mail.ru>
2021-02-07 21:33:39 +0300

    изменил названия некоторых переменных

--- app/src/main/java/com/example/vkinfo/utils/NetworkUtils.java
+++ app/src/main/java/com/example/vkinfo/utils/NetworkUtils.java
@@ -1,7 +1,6 @@
 package com.example.vkinfo.utils;
 
 import android.net.Uri;
-import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,8 +11,8 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class NetworkUtils {
-    private static final String VK_API_BASE_URL = "https://api.vk.com";
-    private static final String VK_API_METHOD = "/method/users.get";
+    private static final String API_BASE_URL = "https://api.vk.com";
+    private static final String API_METHOD = "/method/users.get";
     private static final String PARAM_USER_IDS = "user_ids";
     private static final String PARAM_FIELDS = "fields";
     private static final String PARAM_VERSION = "v";
@@ -21,17 +20,17 @@ public class NetworkUtils {
     private static final String PARAM_LANG = "lang";
 
     /*
-
+    Эта ссылка работать не будет, так как токен устарен
     https://api.vk.com/method/users.get?user_ids=1,2,3,4,5&fields=photo_200&access_token=f9b838c9f9b838c9f9b838c9f4f9ce22e5ff9b8f9b838c999b6f271a1e07182687a7184&v=5.126
 
     */
     public static URL generateURL(String userIds){
-        Uri buildUri = Uri.parse(VK_API_BASE_URL + VK_API_METHOD)
+        Uri buildUri = Uri.parse(API_BASE_URL + API_METHOD)
                 .buildUpon()
                 .appendQueryParameter(PARAM_USER_IDS,userIds)
                 .appendQueryParameter(PARAM_FIELDS,"photo_200")
                 .appendQueryParameter(PARAM_VERSION,"5.126")
-                .appendQueryParameter(PARAM_ACCESS_KEY,"f9b838c9f9b838c9f9b838c9f4f9ce22e5ff9b8f9b838c999b6f271a1e07182687a7184")
+                .appendQueryParameter(PARAM_ACCESS_KEY,"f9b838c9f9b838c9f9b838c9f4f9ce22e5ff9b8f9b838c999b6f271a1e07182687a7184")   //вставьте свой токен
                 .appendQueryParameter(PARAM_LANG,"ru")
                 .build();
         URL url = null;
